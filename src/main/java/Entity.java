be2ac87a568f4bac1f6d057b2fbf1a88847083f5
Ethan <z5369412@unsw.edu.au>
2022-12-02 23:08:39 +1100

    implement stub code

--- src/main/java/Entity.java
+++ src/main/java/Entity.java
@@ -3,4 +3,13 @@ package main.java;
 public abstract class Entity {
     private int id;
     private String name;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
---
+++ src/main/java/area/Area.java
@@ -0,0 +1,5 @@
+package main.java.area;
+
+public abstract class Area {
+    
+}
